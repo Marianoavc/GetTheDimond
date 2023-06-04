e9aff5833a4d43ac0cfc203e0bebf2c3e150b0e6
Mariano Villanueva <mariano.villanueva@unmsm.edu.pe>
2023-06-04 00:59:26 -0500

    version base

--- GetTheDimond.py
+++ GetTheDimond.py
@@ -5,21 +5,16 @@ import random
 pygame.init()
 
 # Definir dimensiones de la ventana
-ANCHO_VENTANA = 800
+ANCHO_VENTANA = 600
 ALTO_VENTANA = 400
 
 # Definir colores
 BLANCO = (255, 255, 255)
 AZUL_CLARO = (0, 191, 255)
-COLOR_CONFIG = (200, 200, 200)  # Color tenue para la ventana de configuraciones
 
 # Calcular tamaño y separación de los cuadrados
 TAMANO_CUADRADO = 80
 ESPACIO_ENTRE_CUADRADOS = 20
-
-# Definir dimensiones de la ventana de configuraciones
-ANCHO_CONFIG = 200
-ALTO_CONFIG = ALTO_VENTANA
 
 # Función para crear lista de cuadrados
 def crear_cuadrados():
@@ -40,9 +35,9 @@ def juego():
     # Crear cuadrados
     cuadrados = crear_cuadrados()
 
-    # Definir número de bombas
-    num_bombas = 1  # Valor predeterminado
-
+    # Colocar bombas en cuadrados aleatorios
+    bombas = random.sample(cuadrados, 1)
+        
     # Cargar imágenes
     imagen_bomba = pygame.image.load("imagesGTD/bomba.png")  # Ruta de la imagen de la bomba
     imagen_diamante = pygame.image.load("imagesGTD/diamante.png")  # Ruta de la imagen del diamante
@@ -52,14 +47,11 @@ def juego():
     imagen_diamante = pygame.transform.scale(imagen_diamante, (TAMANO_CUADRADO, TAMANO_CUADRADO))
 
     # Inicializar lista de imágenes a mostrar en cada cuadro
-    mostrar_cuadrado = [False] * len(cuadrados)
+    mostrar_imagenes = [None] * len(cuadrados)
 
     # Variables del juego
     jugando = True
     bomba_encontrada = False
-
-    # Crear ventana de configuraciones
-    config_rect = pygame.Rect(ANCHO_VENTANA - ANCHO_CONFIG, 0, ANCHO_CONFIG, ALTO_CONFIG)
 
     # Bucle principal del juego
     while jugando:
@@ -67,37 +59,23 @@ def juego():
             if evento.type == pygame.QUIT:
                 jugando = False
             elif evento.type == pygame.MOUSEBUTTONUP:
-                if config_rect.collidepoint(evento.pos):
-                    # Clic en la ventana de configuraciones
-                    if evento.button == 1:
-                        # Botón izquierdo del ratón
-                        if evento.pos[0] > ANCHO_VENTANA - ANCHO_CONFIG + 20 and evento.pos[1] > 40 and evento.pos[1] < 80:
-                            # Clic en el área de configuración de número de bombas
-                            valor_nuevo = pygame_input_dialog("Configuración", "Ingrese el número de bombas:")
-                            try:
-                                num_bombas = int(valor_nuevo)
-                                bombas = random.sample(cuadrados, num_bombas)
-                            except ValueError:
-                                pass
-            else:
                 for i, cuadrado in enumerate(cuadrados):
                     if cuadrado.collidepoint(evento.pos):
                         if cuadrado in bombas:
-                            mostrar_cuadrado[i] = imagen_bomba
+                            # Mostrar imagen de la bomba en el cuadrado
+                            mostrar_imagenes[i] = imagen_bomba
                             bomba_encontrada = True
                         else:
-                            mostrar_cuadrado[i] = imagen_diamante
+                            # Mostrar imagen del diamante en el cuadrado
+                            mostrar_imagenes[i] = imagen_diamante
 
         ventana.fill(BLANCO)  # Rellenar ventana con color blanco
 
         # Dibujar cuadrados y mostrar las imágenes correspondientes
-        for i, cuadrado in enumerate(cuadrados):
+        for cuadrado, imagen in zip(cuadrados, mostrar_imagenes):
             pygame.draw.rect(ventana, AZUL_CLARO, cuadrado)
-            if mostrar_cuadrado[i]:
-                ventana.blit(mostrar_cuadrado[i], cuadrado.topleft)
-
-        # Dibujar ventana de configuraciones
-        pygame.draw.rect(ventana, COLOR_CONFIG, config_rect)
+            if imagen:
+                ventana.blit(imagen, cuadrado.topleft)
 
         pygame.display.update()  # Actualizar ventana
 
@@ -116,42 +94,6 @@ def juego():
 
     # Salir del juego
     pygame.quit()
-
-# Función para mostrar un cuadro de diálogo de entrada
-def pygame_input_dialog(titulo, mensaje):
-    valor = ""
-    entrada_activa = True
-
-    while entrada_activa:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return valor
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    entrada_activa = False
-                elif event.key == pygame.K_BACKSPACE:
-                    valor = valor[:-1]
-                else:
-                    valor += event.unicode
-
-        ventana.fill(BLANCO)
-        fuente = pygame.font.Font(None, 36)
-        texto_titulo = fuente.render(titulo, True, (0, 0, 0))
-        texto_titulo_rect = texto_titulo.get_rect(center=(ANCHO_VENTANA // 2, ALTO_VENTANA // 2 - 50))
-        ventana.blit(texto_titulo, texto_titulo_rect)
-
-        texto_mensaje = fuente.render(mensaje, True, (0, 0, 0))
-        texto_mensaje_rect = texto_mensaje.get_rect(center=(ANCHO_VENTANA // 2, ALTO_VENTANA // 2))
-        ventana.blit(texto_mensaje, texto_mensaje_rect)
-
-        texto_valor = fuente.render(valor, True, (0, 0, 0))
-        texto_valor_rect = texto_valor.get_rect(center=(ANCHO_VENTANA // 2, ALTO_VENTANA // 2 + 50))
-        ventana.blit(texto_valor, texto_valor_rect)
-
-        pygame.display.update()
-
-    return valor
 
 # Ejecutar juego
 juego()
